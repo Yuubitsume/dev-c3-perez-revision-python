5bbacaf33285d5c0821c0a5c6320238586a6c3e6
Yuubitsume <jeannicolas34980@gmail.com>
2023-11-14 17:02:47 +0100

    ajouter le fait d'afficher l'élève avec la plus haute note& C:/Users/Julien/AppData/Local/Microsoft/WindowsApps/python3.11.exe c:/Users/Julien/Documents/Dev_pyth_atl/exercice/ex8.py

--- ex8.py
+++ ex8.py
@@ -1,21 +1,34 @@
-# Création d'un dictionnaire avec des noms, prénoms et notes associées
 notes_eleves = {
     'Alice Smith': 15,
     'Bob Johnson': 12,
     'Claire Williams': 17,
     'Claire Michel': 17,
+    'Claire Alie': 17,
     'David Brown': 14,
-    'Emma Davis': 16
+    'Emma Davis': 16,
+    'Frank Thomas': 15,
+    'George Harris': 13,
+    'Helen Taylor': 16,
+    'Isabel Clark': 14,
+    'Jackie Lee': 18,
+    'Kate Turner': 17,
+    'Liam Walker': 16,
+    'Mia Garcia': 15,
+    'Nathan Allen': 17,
+    'Olivia Martinez': 18,
+    'Wendy Green': 15,
+    'Xavier Brooks': 16,
+    'Yvonne Reed': 14,
+    'Zoe Hall': 18
 }
 
 
-notes_max = 0
-nom_notes_max = ''
+# Initialisation des variables pour stocker les informations sur la note maximale
+max_note = 0
+eleves_max_notes = []
 
-for key, valeur in notes_eleves.items():
-    if notes_max <= valeur:
-        notes_max = valeur
-        nom_notes_max = key
-    
-print(notes_eleves)
-print(notes_max, nom_notes_max)
+# Parcourir le dictionnaire pour trouver la note maximale
+for i, (eleve, note) in enumerate(notes_eleves.items()):
+    if note > max_note:
+        max_note = note
+       
