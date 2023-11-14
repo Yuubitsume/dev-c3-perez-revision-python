15db0e44e7f7bfea2e4f3b269281a6e32e42b261
Yuubitsume <jeannicolas34980@gmail.com>
2023-11-14 16:08:04 +0100

    add lire le fichier input.txt

---
+++ ex6.py
@@ -0,0 +1,7 @@
+import datetime
+
+# Récupérer l'heure actuelle ainsi que la date
+date_actuelle = datetime.datetime.now()
+
+# Afficher la date et l'heure
+print("Date actuelle précisément : ", date_actuelle)
---
+++ ex7.py
@@ -0,0 +1,5 @@
+# Lire le fichier texte input.txt
+with open('C:/Users/Julien/Documents/Dev_pyth_atl/input.txt', 'r', encoding="utf-8") as fichier:
+    contenu = fichier.read()
+    print(contenu)
+
