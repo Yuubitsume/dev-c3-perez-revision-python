47b79fe5af7da1da191ad18aa06fc45a6d5f1a87
Yuubitsume <jeannicolas34980@gmail.com>
2023-11-14 16:16:47 +0100

    add écrire le résultat dans un autre fichier output

--- ex7.py
+++ ex7.py
@@ -6,4 +6,7 @@ with open('C:/Users/Julien/Documents/Dev_pyth_atl/input.txt', 'r', encoding="utf
 # Compte le nombre de mots
 mots = contenu.split()
 nombre_mots = len(mots)
-print(nombre_mots)
+
+# Écrire le résultat dans un autre fichier output.txt
+with open('C:/Users/Julien/Documents/Dev_pyth_atl/output.txt', 'w', encoding="utf-8") as file_output:
+    file_output.write(f'Le nombre de mots dans le fichier est : {nombre_mots}')
