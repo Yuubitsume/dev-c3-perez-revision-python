14bae296e1fc88f91d86965ad9fa1e2b6fc6ee50
Yuubitsume <jeannicolas34980@gmail.com>
2023-11-14 16:17:33 +0100

    add dictionnaire

---
+++ ex8.py
@@ -0,0 +1,11 @@
+# Création d'un dictionnaire avec des noms, prénoms et notes associées
+notes_eleves = {
+    'Alice Smith': 15,
+    'Bob Johnson': 12,
+    'Claire Williams': 17,
+    'Claire Michel': 17,
+    'David Brown': 14,
+    'Emma Davis': 16
+}
+
+print(notes_eleves)
